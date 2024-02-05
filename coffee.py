e4b00123b5b192a7f14932d46ddd7dd6a8711801
Priscillah <nalubegapriscillah@gmail.com>
2024-02-05 19:41:43 +0300

    better

--- coffee.py
+++ coffee.py
@@ -1,1 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def about():
+    return render_template('contant.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+if __name__ == '__main__':
+     app.run(debug = True)
