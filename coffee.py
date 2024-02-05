27f6d087f2eb8c9077fbf9dbdb1fbd8574120b41
Priscillah <nalubegapriscillah@gmail.com>
2024-02-05 14:48:02 +0300

    nice try

---
+++ coffee.py
@@ -0,0 +1,12 @@
+# Simple Flask Application
+from Flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
